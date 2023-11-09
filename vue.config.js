cd8e30e7b7cb60e246b2d96debec508adf6a7d44
devennazareCT <deven@clevertap.com>
2023-11-09 17:00:49 +0530

    Initial deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vueJsCT/' : '/',
+
 })
 // const { defineConfig } = require('@vue/cli-service')
 // const path = require('path');
